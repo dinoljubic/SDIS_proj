e43be5aa7949f3f6ac60ff12fae07abd496cfa24
Nowen <adamczak.nowen.12@gmail.com>
2019-12-03 20:07:43 +0000

    led blinkind with udp messages 2

--- arduino_code/UDP-ESP8266/UDP-ESP8266.ino
+++ arduino_code/UDP-ESP8266/UDP-ESP8266.ino
@@ -8,6 +8,7 @@ WiFiUDP Udp;
 unsigned int  localUdpPort = 4444;  // local port to listen on
 char          incomingPacket[255];  // buffer for incoming packets
 char          replyPacket[255] ;  // a reply string to send back
+char          message[255];
 
 int ss_time_from_server = 0;
 int mm_time_from_server = 0;
@@ -16,7 +17,11 @@ int hh_time_from_server = 0;
 unsigned long external_time = 0;
 unsigned long internal_time = 0;
 
-char message[255];
+int interval=1000;
+unsigned long previousMillis=0;
+unsigned int previousTimeFromUDP =0;
+
+boolean sync = true;
 
 void setup()
 {
@@ -37,37 +42,55 @@ void setup()
   Udp.begin(localUdpPort);
   Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
   Serial.printf("RTC time = %d\n",system_get_rtc_time());
+  
 }
 
 
 void loop(){
-  internal_time = millis();
+  unsigned long currentMillis = millis();
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
+    Serial.println("-------------------------------------------");
     // receive incoming UDP packets
-    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
     int len = Udp.read(incomingPacket, 255);
     if (len > 0)incomingPacket[len] = 0;
     
-    Serial.printf("UDP packet contents at %ld: %s\n", internal_time,incomingPacket);
+    Serial.printf("UDP packet contents at %ld: %s\n", currentMillis,incomingPacket);
     sscanf(incomingPacket,"%d:%d:%d -- %s",&hh_time_from_server,&mm_time_from_server,&ss_time_from_server,message);
-    Serial.printf("Message received --> %s\n",message);
-    
-    if(strcmp(message,"ON")){
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else if (strcmp(message,"OFF")){
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-    
     // send back a reply, to the IP address and port we got the packet from
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     
     Serial.printf("Time from UDP %d:%d:%d \n", hh_time_from_server,mm_time_from_server,ss_time_from_server);
     sprintf(replyPacket,"int_time = %f ext_time = %f",(float)internal_time/1000,(float)ss_time_from_server);
+
+    Serial.printf("currentMillis = %ld\n",currentMillis);
+    Serial.printf("previousMillis = %ld\n",previousMillis);
+    
+    Serial.printf("diff intern = %ld\n",currentMillis-previousMillis);
+    Serial.printf("diff UDP    = %ld\n",ss_time_from_server-previousTimeFromUDP);
+    previousTimeFromUDP=ss_time_from_server;
+    
+    (void) toggleLED(message);
+    previousMillis = currentMillis;
+   
+    
     Udp.write(replyPacket);
     Udp.endPacket();
+    Serial.println("-------------------------------------------");
+  }
+}
+
+void toggleLED(char* message){
+  Serial.printf("Message received --> %s\n",message);
+  if(strcmp(message,"ON")){
+      digitalWrite(LED_BUILTIN, HIGH);
+      sync=true;
+  }
+  else if (strcmp(message,"OFF")){
+    digitalWrite(LED_BUILTIN, LOW);
+    sync=false;
   }
 }
 
